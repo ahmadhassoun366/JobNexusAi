1174593c2357ca6bf52a6bd5b599896bf09d2e99
ahmadhasssoun366 <ahmadhss366@gmail.com>
2023-06-04 01:33:52 +0300

    Your commit message

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Footer from './Components/Footer.jsx'
+import Header from './Components/Header.jsx';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Header/>
+      {/* <h1 className='text-5xl text-red-500'>App</h1> */}
+      <Footer/>
+      </div>
   );
 }
 
---
+++ frontend/src/Pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+const Home = () => {
+  return <>
+  {/* _____________Hero Section Start ______________ */}
+
+
+ 
+  {/* _____________Hero Section end ______________ */} 
+   
+  
+  </>
+  }
+
+export default Home
