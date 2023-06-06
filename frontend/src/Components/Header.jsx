126bd802fa9a73382a886c762d29d10a1b716c2d
ahmadhasssoun366 <ahmadhss366@gmail.com>
2023-06-07 00:26:15 +0300

    Adding Parrallex Animation for the hero section

--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -20,10 +20,9 @@ const Header = () => {
           <div className="flex flex-wrap items-center justify-between gap-6 md:py-3 md:gap-0 lg:py-5">
             <div className="w-full items-center flex justify-between lg:w-auto">
               <img src={Logo} className="mr-5 h-8 sm:h-10" alt="logo" />
-              <span className="self-center text-xl text-white  first-letter:font-semibold whitespace-nowrap dark:text-white">JobNexus <span className='text-xl text-teal-900 font-bold'>Ai</span></span>
+              <span className="self-center text-xl text-white  first-letter:font-semibold whitespace-nowrap dark:text-white">JobNexus <span className='text-2xl font-bold'>Ai</span></span>
 
               <label
-                htmlFor="hbr"
                 className="peer-checked:hamburger text-white first-letter:block relative z-20 p-6 -mr-6 cursor-pointer lg:hidden"
               >
                 <div
@@ -56,7 +55,7 @@ const Header = () => {
                     </Link>
                   </li>
                   <li>
-                  <a href="#" className="block md:px-4 transition hover:text-primary dark:hover:text-primaryLight">
+                  <Link to='' className="block md:px-4 transition hover:text-primary dark:hover:text-primaryLight">
                       <button
                         id="mega-menu-full-cta-image-button"
                         onMouseOver={toggleDropdown}
@@ -76,20 +75,49 @@ const Header = () => {
                           ></path>
                         </svg>
                       </button>
-                    </a>
+                    </Link>
                   </li>
                 </ul>
               </div>
+          
+                 {/* search bar */}
+
+              <div className="pt-2 mr-5 relative mx-auto text-gray-600">
+                      <input
+                        className="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
+                        type="search"
+                        name="search"
+                        placeholder="Search"
+                      />
+                      <button type="submit" className="absolute right-0 top-0 mt-5 mr-4">
+                        <svg
+                          className="text-gray-600 h-4 w-4 fill-current"
+                          xmlns="http://www.w3.org/2000/svg"
+                          version="1.1"
+                          id="Capa_1"
+                          x="0px"
+                          y="0px"
+                          viewBox="0 0 56.966 56.966"
+                          style={{ enableBackground: 'new 0 0 56.966 56.966' }}
+                          width="512px"
+                          height="512px"
+                        >
+                          <path
+                            d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z"
+                          />
+                        </svg>
+                      </button>
+                    </div>
 
               <div className="w-full space-y-2 border-primary/10 dark:border-gray-700 flex flex-col -ml-1 sm:flex-row lg:space-y-0 md:w-max lg:border-l">
-                <a
-                  href="/register"
+                <Link
+                  to="/register"
                   className="relative flex h-9 ml-auto items-center justify-center sm:px-6 before:absolute before:inset-0 before:rounded-full focus:before:bg-sky-600/10 dark:focus:before:bg-sky-400/10 before:transition before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95"
                 >
                   <span className="relative text-sm font-semibold text-primary dark:text-primaryLight">
                     Sign Up
                   </span>
-                </a>
+                </Link>
                 <Link
                 to='/login'
                   className="relative flex h-9 ml-auto items-center justify-center sm:px-6 before:absolute before:inset-0 before:rounded-full before:bg-gray-800 dark:before:bg-sky-400 before:transition before:duration-300 hover:before:scale-105 active:duration-75 active:before:scale-95"
@@ -111,50 +139,50 @@ const Header = () => {
           <div className="grid max-w-screen-xl px-4 py-5 mx-auto text-sm text-gray-500 dark:text-gray-400 md:grid-cols-3 md:px-6">
             <ul className="hidden mb-4 space-y-4 md:mb-0 md:block" aria-labelledby="mega-menu-full-image-button">
               <li>
-                <a href="#" className="hover:underline hover:text-blue-600 dark:hover:text-blue-500">
+                <Link to="#" className="hover:underline hover:text-blue-600 dark:hover:text-blue-500">
                   Online Stores
-                </a>
+                </Link>
               </li>
               <li>
-                <a href="#" className="hover:underline hover:text-blue-600 dark:hover:text-blue-500">
+                <Link to="#" className="hover:underline hover:text-blue-600 dark:hover:text-blue-500">
                   Segmentation
-                </a>
+                </Link>
               </li>
               <li>
-                <a href="#" className="hover:underline hover:text-blue-600 dark:hover:text-blue-500">
+                <Link to="#" className="hover:underline hover:text-blue-600 dark:hover:text-blue-500">
                   Marketing CRM
-                </a>
+                </Link>
               </li>
               <li>
-                <a href="#" className="hover:underline hover:text-blue-600 dark:hover:text-blue-500">
+                <Link to="#" className="hover:underline hover:text-blue-600 dark:hover:text-blue-500">
                   Online Stores
-                </a>
+                </Link>
               </li>
             </ul>
             <ul className="mb-4 space-y-4 md:mb-0">
               <li>
-                <a href="#" className="hover:underline hover:text-blue-600 dark:hover:text-blue-500">
+                <Link to="#" className="hover:underline hover:text-blue-600 dark:hover:text-blue-500">
                   Our Blog
-                </a>
+                </Link>
               </li>
               <li>
-                <a href="#" className="hover:underline hover:text-blue-600 dark:hover:text-blue-500">
+                <Link to="#" className="hover:underline hover:text-blue-600 dark:hover:text-blue-500">
                   Terms & Conditions
-                </a>
+                </Link>
               </li>
               <li>
-                <a href="#" className="hover:underline hover:text-blue-600 dark:hover:text-blue-500">
+                <Link to="#" className="hover:underline hover:text-blue-600 dark:hover:text-blue-500">
                   License
-                </a>
+                </Link>
               </li>
               <li>
-                <a href="#" className="hover:underline hover:text-blue-600 dark:hover:text-blue-500">
+                <Link to="#" className="hover:underline hover:text-blue-600 dark:hover:text-blue-500">
                   Resources
-                </a>
+                </Link>
               </li>
             </ul>
-            <a
-              href="#"
+            <Link
+              to="#"
               className="p-8 text-left bg-local bg-gray-500 bg-center bg-no-repeat bg-cover rounded-lg bg-blend-multiply hover:bg-blend-soft-light dark:hover:bg-blend-darken"
               style={{ backgroundImage: 'url(https://www.windsoracademytrust.org.uk/assets/hero-images/Job-Vacancies-and-Careers/windsor-academy-trust-job-vacancies__FocusFillWzEyMDAsNjc1LCJ4IiwyNjdd.jpeg)' }}
             >
@@ -179,7 +207,7 @@ const Header = () => {
                   ></path>
                 </svg>
               </button>
-            </a>
+            </Link>
           </div>
         </div>
       )}
--- frontend/src/Shared/Services.jsx
+++ frontend/src/Shared/Services.jsx
@@ -1,83 +1,79 @@
-import React from 'react'
-import { BsLightbulbFill , } from 'react-icons/bs';
-import { FaPoll,FaMedal } from 'react-icons/fa';
-
+import React from 'react';
+import { BsLightbulbFill } from 'react-icons/bs';
+import { FaPoll, FaRobot } from 'react-icons/fa';
 
 const Services = () => {
   return (
-<section className="pb-20 relative block bg-gray-900">
-          <div
-            className="bottom-auto top-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden -mt-20"
-            style={{ height: "80px" }}
-          >
-            <svg
-              className="absolute bottom-0 overflow-hidden"
-              xmlns="http://www.w3.org/2000/svg"
-              preserveAspectRatio="none"
-              version="1.1"
-              viewBox="0 0 2560 100"
-              x="0"
-              y="0"
-            >
-              <polygon
-                className="text-gray-900 fill-current"
-                points="2560 0 2560 100 0 100"
-              ></polygon>
-            </svg>
-          </div>
+    <section className="pb-20 relative block bg-gray-900">
+      <div
+        className="bottom-auto top-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden -mt-20"
+        style={{ height: "80px" }}
+      >
+        <svg
+          className="absolute bottom-0 overflow-hidden"
+          xmlns="http://www.w3.org/2000/svg"
+          preserveAspectRatio="none"
+          version="1.1"
+          viewBox="0 0 2560 100"
+          x="0"
+          y="0"
+        >
+          <polygon
+            className="text-gray-900 fill-current"
+            points="2560 0 2560 100 0 100"
+          ></polygon>
+        </svg>
+      </div>
 
-          <div className="container mx-auto px-4 lg:pt-24 lg:pb-64">
-            <div className="flex flex-wrap text-center justify-center">
-              <div className="w-full lg:w-6/12 px-4">
-                <h2 className="text-4xl font-semibold text-white">
-                  Build something
-                </h2>
-                <p className="text-lg leading-relaxed mt-4 mb-4 text-gray-500">
-                  Put the potentially record low maximum sea ice extent tihs year down to low ice.
-                  According to the National Oceanic and Atmospheric Administration, Ted, Scambos.
-                </p>
-              </div>
-            </div>
-            <div className="flex flex-wrap mt-12 justify-center">
-              <div className="w-full lg:w-3/12 px-4 text-center">
-                <div className="text-yellow-600 p-3 w-12 h-12 shadow-lg rounded-full bg-white inline-flex items-center justify-center">
-                  <FaMedal size={25}/>
-                </div>
-                <h6 className="text-xl mt-5 font-semibold text-white">
-                  Excelent Services
-                </h6>
-                <p className="mt-2 mb-4 text-gray-500">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-              </div>
-              <div className="w-full lg:w-3/12 px-4 text-center">
-                <div className="text-red-600 p-3 w-12 h-12 shadow-lg rounded-full bg-white inline-flex items-center justify-center">
-                  <FaPoll size={25}/>
-                </div>
-                <h5 className="text-xl mt-5 font-semibold text-white">
-                  Grow your market
-                </h5>
-                <p className="mt-2 mb-4 text-gray-500">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-              </div>
-              <div className="w-full lg:w-3/12 px-4 text-center">
-                <div className="text-blue-600 p-3 w-12 h-12 shadow-lg rounded-full bg-white inline-flex items-center justify-center">
-                  <BsLightbulbFill size={25}/>
-                </div>
-                <h5 className="text-xl mt-5 font-semibold text-white">
-                  Launch time
-                </h5>
-                <p className="mt-2 mb-4 text-gray-500">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-              </div>
-            </div>
+      <div className="container mx-auto px-4 lg:pt-24 lg:pb-64">
+        <div className="flex flex-wrap text-center justify-center">
+          <div className="w-full lg:w-6/12 px-4">
+            <h2 className="text-4xl font-semibold text-white">
+              Transforming Recruitment with AI
+            </h2>
+            <p className="text-lg leading-relaxed mt-4 mb-4 text-gray-500">
+              Harness the power of artificial intelligence to revolutionize the recruitment process. Streamline and optimize your hiring procedures like never before.
+            </p>
           </div>
-        </section>  )
+        </div>
+        <div className="flex flex-wrap mt-12 justify-center">
+          <div className="w-full lg:w-3/12 px-4 text-center">
+            <div className="text-yellow-600 p-3 w-12 h-12 shadow-lg rounded-full bg-white inline-flex items-center justify-center">
+              <FaRobot size={25} />
+            </div>
+            <h6 className="text-xl mt-5 font-semibold text-white">
+              Intelligent Candidate Screening
+            </h6>
+            <p className="mt-2 mb-4 text-gray-500">
+              Leverage AI algorithms to analyze resumes and identify the most qualified candidates efficiently.
+            </p>
+          </div>
+          <div className="w-full lg:w-3/12 px-4 text-center">
+            <div className="text-red-600 p-3 w-12 h-12 shadow-lg rounded-full bg-white inline-flex items-center justify-center">
+              <FaPoll size={25} />
+            </div>
+            <h5 className="text-xl mt-5 font-semibold text-white">
+              Data-driven Decision Making
+            </h5>
+            <p className="mt-2 mb-4 text-gray-500">
+              Utilize data analytics and AI insights to make informed hiring decisions and optimize your recruitment strategy.
+            </p>
+          </div>
+          <div className="w-full lg:w-3/12 px-4 text-center">
+            <div className="text-blue-600 p-3 w-12 h-12 shadow-lg rounded-full bg-white inline-flex items-center justify-center">
+              <BsLightbulbFill size={25} />
+            </div>
+            <h5 className="text-xl mt-5 font-semibold text-white">
+              Streamlined Hiring Process
+            </h5>
+            <p className="mt-2 mb-4 text-gray-500">
+              Automate repetitive tasks and eliminate bottlenecks to accelerate the hiring process and improve overall efficiency.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default Services
+export default Services;
