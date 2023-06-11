b5f199e26aa7c77fc185fb74d5dfd110f897d4f4
ahmadhasssoun366 <ahmadhss366@gmail.com>
2023-06-11 20:08:32 +0300

    Changing LoginPage and Registration page. In addtion to adding new navbar

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -15,7 +15,7 @@ const Header = () => {
 
   return (
     <header>
-      <nav className="fixed z-20 w-full text-white dark:bg-gray-900/80  navbar  dark:border-gray-800 peer-checked:navbar-active dark:shadow-none">
+      <nav className="fixed z-20 w-full text-white bg-gray-500/80  navbar  dark:border-gray-800  dark:shadow-none">
         <div className="xl:container m-auto px-6 md:px-12 lg:px-6">
           <div className="flex flex-wrap items-center justify-between gap-6 md:py-3 md:gap-0 lg:py-5">
             <div className="w-full items-center flex justify-between lg:w-auto">
@@ -37,11 +37,11 @@ const Header = () => {
             </div>
             <div className="navmenu hidden w-full flex-wrap justify-between items-center mb-16 space-y-8 p-6 border border-gray-100 rounded-3xl shadow-2xl shadow-gray-300/20 bg-white dark:bg-gray-800 lg:space-y-0 lg:p-0 lg:m-0 lg:flex md:flex-nowrap lg:bg-transparent lg:w-7/12 lg:shadow-none dark:shadow-none dark:border-gray-700 lg:border-0">
               <div className="text-white dark:text-gray-300 lg:pr-4">
-                <ul className="space-y-6 tracking-wide font-medium text-l lg:text-sm lg:flex lg:space-y-0">
+                <ul className="space-y-6 tracking-wide font-medium text-base lg:text-sm lg:flex lg:space-y-0">
                   <li>
                     <Link
                       to='/'
-                      className="block md:px-4 text-base	underline underline-offset-8		 transition hover:text-primary dark:hover:text-primaryLight"
+                      className="block md:px-4 transition hover:text-primary dark:hover:text-primaryLight"
                     >
                       <span>Home</span>
                     </Link>
@@ -49,13 +49,13 @@ const Header = () => {
                   <li>
                     <Link
                       to='/blogs'
-                      className="block md:px-4 text-base 	underline underline-offset-8	 transition hover:text-primary dark:hover:text-primaryLight"
+                      className="block md:px-4 transition hover:text-primary dark:hover:text-primaryLight"
                     >
                       <span>Blog</span>
                     </Link>
                   </li>
                   <li>
-                  <Link to='' className="block md:px-4 text-base underline underline-offset-8		 transition hover:text-primary dark:hover:text-primaryLight">
+                  <Link to='' className="block md:px-4 transition hover:text-primary dark:hover:text-primaryLight">
                       <button
                         id="mega-menu-full-cta-image-button"
                         onMouseOver={toggleDropdown}
@@ -80,34 +80,7 @@ const Header = () => {
                 </ul>
               </div>
           
-                 {/* search bar */}
-
-              <div className="pt-2 mr-5 relative mx-auto text-gray-600">
-                      <input
-                        className="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
-                        type="search"
-                        name="search"
-                        placeholder="Search"
-                      />
-                      <button type="submit" className="absolute right-0 top-0 mt-5 mr-4">
-                        <svg
-                          className="text-gray-600 h-4 w-4 fill-current"
-                          xmlns="http://www.w3.org/2000/svg"
-                          version="1.1"
-                          id="Capa_1"
-                          x="0px"
-                          y="0px"
-                          viewBox="0 0 56.966 56.966"
-                          style={{ enableBackground: 'new 0 0 56.966 56.966' }}
-                          width="512px"
-                          height="512px"
-                        >
-                          <path
-                            d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z"
-                          />
-                        </svg>
-                      </button>
-                    </div>
+             
 
               <div className="w-full space-y-2 border-primary/10 dark:border-gray-700 flex flex-col -ml-1 sm:flex-row lg:space-y-0 md:w-max lg:border-l">
                 <Link
--- frontend/src/Pages/Register.jsx
+++ frontend/src/Pages/Register.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import Header from '../Components/Header';
-import SmallFooter from '../Components/SmallFooter';
+import Navbar from '../Components/Navbar';
+import Footer from '../Components/Footer';
 
 const Register = () => {
   const [accountType, setAccountType] = useState(null);
@@ -44,6 +44,7 @@ const Register = () => {
 
   return (
     <>
+    <Navbar />
     <section className="bg-gray-900 dark:bg-gray-900">
 
       <div className="flex  bg-gray-100 justify-center min-h-screen">
@@ -51,11 +52,11 @@ const Register = () => {
           className="hidden bg-cover lg:block lg:w-2/5"
           style={{
             backgroundImage:
-              "url('https://images.unsplash.com/photo-1494621930069-4fd4b2e24a11?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=715&q=80')",
+              "url('https://www.resourceondemand.com/wp-content/uploads/2021/04/tech-recruitment-ROD.jpg')",
           }}
         ></div>
 
-        <div className="flex items-center w-full max-w-3xl p-8 mx-auto lg:px-12 lg:w-3/5">
+        <div className="flex items-center w-full p-8 mx-auto lg:px-12 lg:w-3/5 my-24">
           <div className="w-full">
             <h1 className="text-2xl font-semibold tracking-wider text-gray-800 capitalize dark:text-white">
               Get your free account now.
@@ -184,7 +185,7 @@ const Register = () => {
           {!passwordMatch && <p className="text-red-500">Passwords do not match</p>}
         </div>
               <button
-                className="flex items-center justify-between w-full px-6 py-3 text-sm tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-500 rounded-md hover:bg-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50"
+                className="flex items-center justify-between w-full px-6 py-3 text-sm tracking-wide text-white capitalize transition-colors duration-300 transform bg-gray-500 rounded-md hover:bg-gray-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-50"
               >
                 <span>Sign Up </span>
 
@@ -205,7 +206,7 @@ const Register = () => {
           </div>
         </div>
       </div>
-      <SmallFooter className='bg-gray-900'/>
+      <Footer/>
     </section>
     </>
   );
