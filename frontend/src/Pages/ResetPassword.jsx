d3a16737a61bc0eb118f8d356cdd596278373a2a
ahmadhasssoun366 <ahmadhss366@gmail.com>
2023-06-22 00:54:06 +0300

    Editing filed

--- frontend/src/Pages/ResetPassword.jsx
+++ frontend/src/Pages/ResetPassword.jsx
@@ -26,13 +26,24 @@ export const ResetPassword = () => {
 						<form class="px-8 pt-6 pb-8 mb-4 bg-white rounded">
 							<div class="mb-4">
 								<label class="block mb-2 text-sm font-bold text-gray-700" for="email">
-									Email
+									New Password
 								</label>
 								<input
 									class="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-									id="email"
-									type="email"
-									placeholder="Enter Email Address..."
+									id="password"
+									type="password"
+									placeholder="New password..."
+								/>
+							</div>
+                            <div class="mb-4">
+								<label class="block mb-2 text-sm font-bold text-gray-700" for="email">
+									Confirm New Password
+								</label>
+								<input
+									class="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+									id="c_password"
+									type="password"
+									placeholder="Confrim New Password..."
 								/>
 							</div>
 							<div class="mb-6 text-center">
